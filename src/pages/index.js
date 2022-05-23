2f5f81725052d0824266d1a417c8df35d3bf1b8e
mohsen <mnq13800831@gmail.com>
2022-05-24 01:23:06 +0430

    add banner to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
 import Layout from "../components/common/layout";
+import Banner from "../components/page/banner";
+
 export default function Home() {
-  return <Layout>{/*children*/}</Layout>;
+  return (
+    <Layout>
+      <Banner />
+    </Layout>
+  );
 }
