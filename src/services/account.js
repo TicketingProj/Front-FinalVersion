3c616ad983ac496d08383e845078a0aa23ad8d17
mohsen <mnq13800831@gmail.com>
2022-06-15 08:10:26 +0430

    fix account services

--- src/services/account.js
+++ src/services/account.js
@@ -9,6 +9,11 @@ export async function PostPhoneNumber(phoneNumber) {
   return response;
 }
 
+export async function GetSingleUser(id) {
+  const response = await axios.get(`${BASE_URL}account/${id}`);
+  return response;
+}
+
 export async function SmsVarificationOtp({ phoneNumber, otp }) {
   const response = await axios.post(`${BASE_URL}accounts/verify/`, {
     phoneNumber,
@@ -18,12 +23,34 @@ export async function SmsVarificationOtp({ phoneNumber, otp }) {
   return response;
 }
 
-export async function RegisterUser(id, { fullName, email, phoneNumber }) {
-  const response = await axios.patch(`${BASE_URL}/accounts/${id}/`, {
+export async function RegisterUser({
+  avatar,
+  id,
+  fullName,
+  email,
+  phoneNumber,
+  token,
+}) {
+  const data = {
     fullName,
     email,
     phoneNumber,
-  });
+  };
+
+  if (avatar) {
+    console.log("come here");
+    data.avatar = avatar;
+  }
+
+  const response = await axios.patch(
+    `${BASE_URL}/accounts/${id}/`,
+    { ...data },
+    {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    }
+  );
 
   return response;
 }
